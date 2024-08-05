94a44869da007ad16e079a9569b03e6739615714
miriam <paulmiriam@web.de>
2024-08-05 15:46:19 +0900

    neue Gruppe anlegen funktioniert

--- controllers/groupController.js
+++ controllers/groupController.js
@@ -2,23 +2,22 @@ const Group = require("./../models/groupModel");
 const catchAsync = require("./../utils/catchAsync");
 const AppError = require("./../utils/appError");
 
-
 exports.createGroup = catchAsync(async (req, res, next) => {
-    const { groupName, groupMembers } = req.body;
+  const { groupName, groupMembers } = req.body;
 
-    if (!groupName || !groupMembers) {
-        return next(new AppError("Please provide all required fields", 400));
-    }
+  if (!groupName || !groupMembers) {
+    return next(new AppError("Please provide all required fields", 400));
+  }
 
-    const newGroup = {
-        groupName,
-        groupMembers
-    };
+  const newGroup = await Group.create({
+    groupName,
+    groupMembers,
+  });
 
-    res.status(201).json({
-        status: "success",
-        data: {
-            group: newGroup
-        },
-    });
+  res.status(201).json({
+    status: "success",
+    data: {
+      group: newGroup,
+    },
+  });
 });
--- models/groupModel.js
+++ models/groupModel.js
@@ -1,18 +1,20 @@
 const mongoose = require("mongoose");
-const User = require("./userModel");
+// const User = require("./userModel");
 
 const groupSchema = new mongoose.Schema({
-    groupName: {
-      type: String,
-      required: [true, "Please provide a groupname"], 
-      unique: true, // Muss eindeutig sein
+  groupName: {
+    type: String,
+    required: [true, "Please provide a groupname"],
+    unique: true, // Muss eindeutig sein
+  },
+  groupMembers: [
+    {
+      type: mongoose.Schema.Types.Array,
+      ref: "User",
+      unique: false,
     },
-    groupMembers: [{
-        type: mongoose.Schema.Types.Array,
-        ref: "User",
-        unique: false
-    }]
-  });
+  ],
+});
 
 const Group = mongoose.model("Group", groupSchema);
 
